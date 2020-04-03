200b131c227abd6efa21f1f3d7bea39deb45ea99
LAFONTAINE <lafont_a@etna-alternance.net>
2020-04-03 03:09:51 +0200

    readme

--- projet/db.sh
+++ projet/db.sh
@@ -3,4 +3,3 @@
 docker exec -it reucopro_php sh -c "php bin/console doctrine:database:drop --force"
 docker exec -it reucopro_php sh -c "php bin/console doctrine:database:create"
 docker exec -it reucopro_php sh -c "php bin/console doctrine:schema:create"
-docker exec -it reucopro_php sh -c "php -d memory_limit=-1 bin/console doctrine:fixtures:load --no-interaction --env=dev -v"
