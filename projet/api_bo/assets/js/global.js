de918fa8e2941c5766a12c907137973f41decb42
Gabriel Mauny <mauny_g@etna-alternance.net>
2020-04-11 23:27:51 +0200

    * Ajout de la suppression + edition en cours * Ajout de la pagination sur la liste users

--- projet/api_bo/assets/js/global.js
+++ projet/api_bo/assets/js/global.js
@@ -1,6 +1,8 @@
 const $ = require('jquery');
 
 require('bootstrap');
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
 
 $(document).ready(function () {
     $('[data-toggle="popover"]').popover();
