50ad1331ccc7db51fc657957df2a23979f2e3b0c
Raph <obadia_r@etna-alternance.net>
2020-04-13 10:43:29 +0200

    test get (all or id) with random IDs

--- projet/api_bo/tests/HttpApiTest.php
+++ projet/api_bo/tests/HttpApiTest.php
@@ -31,22 +31,95 @@ class HttpApiTest extends ApiTestCase
         $this->assertResponseIsSuccessful();
     }
 
-    public function testGetBuildingsInSyndic(): void
+    public function testGetBuildingsBySyndic(): void
     {
-        $res = static::createClient()->request("POST", "/api/login_check", ['json' => [
-            'Content-type' => 'application/json',
-            "username" => "admin@reucopro.com",
-            "password" => "admin",
-        ]]);
-
-//        $token = json_decode($res->getContent(), true)['token'];
-//        var_dump($token);
+        $id = 0;
         try {
-            $response = static::createClient()->request('GET', '/api/syndics/1/buildings', ['auth_bearer' => $this->token
+            $id = random_int(1, 4);
+        } catch (\Exception $e) {
+        }
+        try {
+            $response = static::createClient()->request('GET', "/api/syndics/{$id}/buildings", ['auth_bearer' => $this->token
             ]);
         } catch (TransportExceptionInterface $e) {
         }
         $this->assertResponseStatusCodeSame(200, "Wrong code, 200 expected");
+    }
+
+    public function testGetOwnersByBuilding(): void
+    {
+        $id = 0;
+        $id2 = 0;
+        try {
+            $id = random_int(1, 100);
+            $id2 = random_int(1, 100);
+        } catch (\Exception $e) {
+        }
+        try {
+            $response = static::createClient()->request('GET', "/api/syndics/{$id}/buildings/{$id2}/owners", ['auth_bearer' => $this->token]);
+        } catch (TransportExceptionInterface $e) {
+        }
+        $this->assertResponseStatusCodeSame(200, "Wrong code, 200 expected");
+    }
+
+    public function testGetResolutionByMeeting(): void
+    {
+        $id = 0;
+        try {
+            $id = random_int(1, 100);
+        } catch (\Exception $e) {
+        }
+        try {
+            $response = static::createClient()->request('GET', "/api/meetings/{$id}/resolutions", ['auth_bearer' => $this->token]);
+        } catch (TransportExceptionInterface $e) {
+        }
+        $this->assertResponseStatusCodeSame('200', "Wrong code, 200 expected");
+    }
+
+    public function testGetDelegationAll(): void
+    {
+        try {
+            $response = static::createClient()->request('GET', '/api/delegations', ['auth_bearer' => $this->token]);
+        } catch (TransportExceptionInterface $e) {
+        }
+        $this->assertResponseStatusCodeSame('200', 'Wrong code 200 expected');
+    }
+
+    public function testGetLotById(): void
+    {
+        $id = 0;
+        try {
+            $id = random_int(1, 100);
+        } catch (\Exception $e) {
+        }
+        try {
+            $response = static::createClient()->request('GET', "/api/lots/{$id}", ['auth_bearer' => $this->token]);
+        } catch (TransportExceptionInterface $e) {
+        }
+        $this->assertResponseStatusCodeSame('200', 'Wrong code, 200 expected');
+    }
+
+    public function testGetUserAll(): void
+    {
+        try {
+            $response = static::createClient()->request('GET', '/api/users', ['auth_bearer' => $this->token]);
+        } catch (TransportExceptionInterface $e) {
+        }
+        $this->assertResponseStatusCodeSame('200', 'Wrong code, 200 expected');
+    }
+
+    public function testGetVoteById(): void
+    {
+        $id = 0;
+        try {
+            $id = random_int(1, 100);
+        } catch (\Exception $e) {
+        }
+        try {
+            $response = static::createClient()->request('GET', "/api/votes/{$id}", ['auth_bearer' => $this->token]);
+        } catch (TransportExceptionInterface $e) {
+        }
+        $this->assertResponseStatusCodeSame('200', 'Wrong code, 200 expected');
     }
 
 }
